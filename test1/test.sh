7187155a79c47ce44765b9b7f3029f0dfda0dc73
Will Tackett <will.s.tackett@gmail.com>
2022-09-19 13:24:43 -0400

    gitignore

--- test1/test.sh
+++ test1/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -ti --rm -v /home/will/Gears/asltlbx-py/test1/data:/opt/base/input:rw --entrypoint=/bin/bash willtack/asltlbx-py:0.0.1
+docker run -ti --rm -v /home/will/Gears/asltlbx-py/test1/data:/opt/base/input:rw --entrypoint=/bin/bash willtack/asltlbx-py:0.0.4
